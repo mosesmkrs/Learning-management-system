572e2dc07f10458aca64ea3f98c6ce393ba7e5c1
mosesmkrs <karisamoses392@gmail.com>
2024-10-10 10:39:09 +0300

    sign in

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -3,9 +3,7 @@ import { SignIn } from '@clerk/clerk-react'
 export default function SignInPage() {
   return (
     <div className='mx-auto mt-10 w-fit'>
-      <SignIn path="/sign-in"
-      fallbackRedirectUrl={'/dashboard'}
-      />
+      <SignIn path="/sign-in"/>
     </div>
   )
 }
